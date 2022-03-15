a7fac75f50fa8ef77303e0ce07d985617adc24a3
reginmaru <reginmaru@gmail.com>
2022-03-15 20:09:36 +0000

    added node class, not added link class so theres a bunch of errors

---
+++ readme.md
@@ -0,0 +1,5 @@
+v0.0
+
+1. Creating directories for routeXML, link and node. Thinking of the next steps... Construct link and node classes first. 
+2. They need to be instantiated so can't use abstract classes.
+3. Haven't yet learned how to attach one - many on java so gonna look that up after I create classes.
---
+++ src/main/java/com/example/RoutingApp/link/link.java
@@ -0,0 +1,31 @@
+package com.example.RoutingApp.link;
+
+public class link {
+    private Long id;
+    private link.getName() startingNode;
+    private link.getName() endingNode;
+    private Double weight;
+
+    public Long getId() {
+        return id;
+    }
+    public link.getName() getStartingNode() {
+        return startingNode;
+    }
+    public link.getName() getEndingNode() {
+        return endingNode;
+    }
+    public Double getWeight() {
+        return weight;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setStartingNode() {
+        this.startingNode = startingNode;
+    }
+    public void setEndingNode() {
+        this.endingNode = endingNode;
+    }
+    
+}
