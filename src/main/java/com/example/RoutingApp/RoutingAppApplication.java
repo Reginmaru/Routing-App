5670e1ad3230b303d50daf6f97428fe54db48dbd
reginmaru <reginmaru@gmail.com>
2022-03-15 19:50:16 +0000

    Initialising springboot with JPA, postgres and web

---
+++ src/main/java/com/example/RoutingApp/RoutingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.RoutingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoutingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RoutingApp/RoutingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RoutingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
